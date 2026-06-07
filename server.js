abbb20a56bf1cd1a5d517bb169b122107836a0dd
lito16loti <marlitobebirothegreat@gmail.com>
2026-06-08 00:44:58 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -40,13 +40,13 @@ const app = express();
 ===================== */
 
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: [
+    "http://127.0.0.1:5500",
+    "frontend-kappa-tawny-98.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
-
-// Handle preflight requests
-app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
