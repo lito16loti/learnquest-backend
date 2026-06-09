fcf67d6d53f375d864371d7bd9a1df414a1f4506
lito16loti <marlitobebirothegreat@gmail.com>
2026-06-10 00:43:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
-    "https://bleeeww.vercel.app"
+    "https://gamifyblue.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
