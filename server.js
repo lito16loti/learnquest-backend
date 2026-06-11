2bd5b2b907507ec0a7726bd03c28be6d1609d950
lito16loti <marlitobebirothegreat@gmail.com>
2026-06-11 21:19:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
-    "frontend-kappa-tawny-98.vercel.app"
+    "https://fixx-psi.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
