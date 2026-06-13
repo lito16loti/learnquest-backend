f7ad1b31a548e03b6e828467950c11badf6a055b
lito16loti <marlitobebirothegreat@gmail.com>
2026-06-13 19:53:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -531,7 +531,7 @@ app.delete("/admin/lessons/:id", verifyToken, isAdmin, async (req, res) => {
 /* =====================
    SAVE QUIZ HISTORY
 ===================== */
-app.post("/quiz-history", verifyToken, async (req, res) => {
+app.post("/quiz-attempts", verifyToken, async (req, res) => {
   try {
     const { score, total, xp_earned, time_taken, subject } = req.body;
 
