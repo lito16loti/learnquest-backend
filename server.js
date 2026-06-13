4776bf24638fb128aebc29a0b629a78c98f37614
lito16loti <marlitobebirothegreat@gmail.com>
2026-06-13 20:34:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
-    "https://3-azure-six.vercel.app"
+    "https://4-one-chi.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
