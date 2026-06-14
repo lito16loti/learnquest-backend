22b264f5b361e4eb9603e085eb7c9dd11acd4d69
lito16loti <marlitobebirothegreat@gmail.com>
2026-06-14 19:38:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
-    "frontend-kappa-tawny-98.vercel.app"
+    "https://g4mify.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
