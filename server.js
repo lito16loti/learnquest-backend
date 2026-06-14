a8346a17f68f3e287c89b0e799c4e8d589de2e2b
lito16loti <marlitobebirothegreat@gmail.com>
2026-06-14 17:33:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
-    "https://yawa-one.vercel.app"
+    "https://silver-lemur-537729.hostingersite.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
